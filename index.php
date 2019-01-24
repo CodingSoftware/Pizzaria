e09eeab1a6299c471183cd64540a8d160082f424
codingsoftware <46891303+codingsoftware@users.noreply.github.com>
2019-01-24 21:24:20 -0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <head>
 </head>
 <body>
-Ola
+<?php
+$banco = "d1vue62a45nmd5";
+$usuario = "hytmsojwrxcwvd";
+$senha = "6fbd732c07b43fc938b9cf0f246650b2d6c5ec5d8457b1c6e06d160e08c619c6";
+$hostname = "ec2-54-243-228-140.compute-1.amazonaws.com:5432";
+$conn = mysql_connect($hostname,$usuario,$senha);?>
 </body>
 </html>
