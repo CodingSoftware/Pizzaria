8b88674734b0bfb8f735862e257625b61b4d549b
codingsoftware <codingsoftware@github.com>
2019-02-05 10:35:26 -0300

    ok

--- _php/mostprod.php
+++ _php/mostprod.php
@@ -1,10 +1,25 @@
+<html>
+<head>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;
+}
+</style>
+</head>
+
 <?php
 require_once 'conector.php';
 
 $acao=$_GET['acao'];
 
 
-$select = "SELECT tamanho FROM tamanho";
+$select = "SELECT tamanho, qtd_fatias ,qtd_sabores , valor FROM tamanho";
 $result = mysqli_query($conectar, $select);
 $result1 = mysqli_query($conectar, $select);
 
@@ -23,17 +38,17 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 if($acao=="deletar"){
-
+echo "<table><tr><th>Tamanho</th> <th>Qnt. Fatias</th> <th>Qnt. Sabores</th> <th>Valor</th> <th>Remoção</th></tr>";
 if (mysqli_num_rows($result1) > 0) {
     // output data of each row
     while($row1 = mysqli_fetch_assoc($result1)) {
-        echo "<tr><td>".$row1["tamanho"]."</td><td><button class='btn btn-danger'>Deletar</button></td><tr>";
+        echo "<tr><td>".$row1["tamanho"]."</td> <td>".$row1["qtd_fatias"]."</td> <td>".$row1["qtd_sabores"]."</td> <td>".$row1["valor"]."</td> <td><button class='btn btn-sm btn-danger'>Deletar</button></td>";
     }
 
 } else {
     echo ". results";
 }
-
+echo "</table>";
 
 }
 
@@ -43,3 +58,4 @@ if (mysqli_num_rows($result1) > 0) {
 
 mysqli_close($conectar);
 ?>
+</html>
--- produtos.php
+++ produtos.php
@@ -77,6 +77,7 @@ small {
 <div class="modal-content">
 <div class="modal-header">
 <h5 class="modal-title" id="exampleModalLabel">Delete os Tamanhos</h5>
+
 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 <span aria-hidden="true">&times;</span>
 </button>
