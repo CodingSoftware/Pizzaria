b9105350a6867cf04640eadb5b3426ec0bee6bc1
codingsoftware <codingsoftware@github.com>
2019-02-11 13:48:20 -0300

    ok

--- _php/teste/insert.php
+++
@@ -1,9 +0,0 @@
-<?php require_once('_php/conector.php');
-
-
-
-$inserir = "INSERT INTO preco(cliente,sabor1) VALUES('Marcos','2')";
-mysqli_query($conectar,$inserir) or die("Erro ao tentar cadastrar registro");
-mysqli_close($conectar);
-echo "Cliente cadastrado com sucesso!";
-?>
--- header.php
+++ header.php
@@ -16,13 +16,16 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="css/navbar.css">
-<link rel="stylesheet" href="css/style.css">
 
-    <!-- Custom styles for this template -->
+<link href="css/carousel.css" rel="stylesheet">
 
+
+    <style>
+
+    </style>
   </head>
   <body>
-  <header>
+    <header>
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger">
     <a class="navbar-brand" href="#"> <i class="fas fa-utensils"></i>Pizzaria</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,51 +36,54 @@
         <li class="nav-item">
           <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
         </li>
-		<li class="nav-item">
+    <li class="nav-item">
           <a class="nav-link" href="#">Cardápio</a>
         </li>
-		<li class="nav-item">
+    <li class="nav-item">
           <a class="nav-link" href="monte-sua-pizza.php">Monte sua Pizza</a>
         </li>
-		<li class="nav-item">
+    <li class="nav-item">
           <a class="nav-link" href="contato.html">Contato</a>
         </li>
-      		 <li class="nav-item">
+           <li class="nav-item">
           <a class="nav-link" href="cadastro.html">Cadastre-se</a>
         </li>
-			<li class="nav-item">
-				<a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#">Login</a>
-				<ul class="dropdown-menu form-wrapper">
-					<li>
-						<form action="/examples/actions/confirmation.php" method="post">
-							<p class="hint-text">Faça o seu login usando as Redes Sociais</p>
-							<div class="form-group social-btn clearfix">
-								<a href="#" class="btn btn-primary pull-left"><i class="fab fa-facebook"></i> Facebook</a>
-								<a href="#" class="btn btn-info pull-right"><i class="fab fa-twitter"></i> Twitter</a>
-							</div>
-							<div class="or-seperator"><b>ou</b></div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Email" required="required">
-							</div>
-							<div class="form-group">
-								<input type="password" class="form-control" placeholder="Senha" required="required">
-							</div>
-							<input type="submit" class="btn btn-success btn-block" value="Logar">
-							<div class="form-footer">
-								<a href="#">Esqueceu a senha?</a>
+      <li class="nav-item">
+        <a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#">Login</a>
+        <ul class="dropdown-menu form-wrapper">
+          <li>
+            <form action="/examples/actions/confirmation.php" method="post">
+              <p class="hint-text">Faça o seu login usando as Redes Sociais</p>
+              <div class="form-group social-btn clearfix">
+                <a href="#" class="btn btn-primary pull-left"><i class="fab fa-facebook"></i> Facebook</a>
+                <a href="#" class="btn btn-info pull-right"><i class="fab fa-twitter"></i> Twitter</a>
+              </div>
+              <div class="or-seperator"><b>ou</b></div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email" required="required">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" placeholder="Senha" required="required">
+              </div>
+              <input type="submit" class="btn btn-success btn-block" value="Logar">
+              <div class="form-footer">
+                <a href="#">Esqueceu a senha?</a>
 
-							</div>
+              </div>
 
-						</form>
-					</li>
-				</ul>
-		 </li>
+            </form>
+          </li>
+        </ul>
+     </li>
 
       </ul>
 
   </nav>
 
 </header>
-<br><br><br><br>
+
+
+
+
 </body>
 </html>
