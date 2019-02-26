d4e1f0c88fb589d9afb2b25782859a76d8eee182
codingsoftware <codingsoftware@github.com>
2019-02-26 18:02:02 -0300

    Revert "Merge branch 'master' of https://github.com/coding-software/Pizzaria"
    This reverts commit 70a83b4079c810da3bc028f99a038fb39200ee24, reversing
    changes made to 1c85753ddc6663bcd7fd2b2dd0f0f319611a3598.

--- _php/conector.php
+++ _php/conector.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$servidor = "etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$usuario = "j8uju4txtsb8tli2";
-$senha = "z5v0aaon2iijb3lr";
-$banco = "b98p7h43wvkg7xwp";
+$servidor = "localhost";
+$usuario = "filial185";
+$senha = "senhafilial";
+$banco = "test";
 $conectar = mysqli_connect("$servidor","$usuario","$senha","$banco");
 
 if (!$conectar) {
