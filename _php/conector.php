6a795b3b4c89f2c93f42d49d9be0c68fb55b1f55
Marcos Reis <codingsoftware@github.com>
2019-02-28 16:53:33 -0300

    Git solucionado
    Combo 1 configuração para casa ou heroku
    
    Combo 2 Configuração atacadão

--- _php/conector.php
+++ _php/conector.php
@@ -1,10 +1,25 @@
 <?php
 
+$combo=1;
 
-$servidor = "etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$usuario = "j8uju4txtsb8tli2";
-$senha = "z5v0aaon2iijb3lr";
-$banco = "b98p7h43wvkg7xwp";
+
+
+if($combo==1){
+  $servidor = "etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+  $usuario = "j8uju4txtsb8tli2";
+  $senha = "z5v0aaon2iijb3lr";
+  $banco = "b98p7h43wvkg7xwp";
+
+}
+
+if($combo==2){
+  $servidor = "localhost";
+  $usuario = "filial185";
+  $senha = "senhafilial";
+  $banco = "test";
+
+}
+
 $conectar = mysqli_connect("$servidor","$usuario","$senha","$banco");
 
 if (!$conectar) {
@@ -15,7 +30,7 @@ if (!$conectar) {
 }
 else{
 
-echo "ok";
+
 }
 
 
