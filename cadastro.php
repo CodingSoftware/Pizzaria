47f1d879d061dfe6ae2990e2cb045b1039d5decc
codingsoftware <46891303+codingsoftware@users.noreply.github.com>
2019-03-04 10:19:03 -0300

    04.03.19
    Título do site

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.5">
-    <title>Checkout example · Bootstrap</title>
+    <title>Cadastro</title>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
